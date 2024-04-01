7731e38df71be3e9ab37613f364c63d84354c057
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-01 19:36:43 +0530

    updated

--- 01_Basic/01_Conventions.mjs
+++ 01_Basic/01_Conventions.mjs
@@ -2,17 +2,20 @@
     Here is an example: We use the decipher object, just like we did with the cipher object in the previous
     method, invoking decipher. Update with the ciphertext to decrypt and use decipher.final when we're done.
 */
+import crypto from 'crypto';
 
-const crypto = require('crypto')
-const fs = require('fs')
-const util = require('util')
-const readFile = util.promisify(fs.readFile)
+// Key and IV setup
+// AES-256-CBC uses 32 byte keys and 16 byte IVs
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
-const bobPublicKeyPem = bobKeyPair.publicKey.export(
-    {type: 'spki', format: 'pem'}
-)
-    
-const aliceSharedSecret = crypto.diffieHellman({
-    publicKey: crypto.createPublicKey(bobPublicKeyPem),
-    privateKey: aliceKeyPair.privateKey
-})
+// The plain text message to be encrypted
+const plainText = "This is a plain text message!";
+
+// Create a cipher object ("aes-256-cbc" this algorithm is used for encryption. This works as a block cipher)
+const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+
+let encrypted = cipher.update(plainText, 'utf8', 'hex');
+encrypted += cipher.final('hex');
+
+console.log('Encrypted: ', encrypted);
