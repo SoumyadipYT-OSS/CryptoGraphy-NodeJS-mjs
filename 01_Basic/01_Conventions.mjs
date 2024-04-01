ecbefbc864e6e20554faf0afa092cfa9aacadd97
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-01 19:19:44 +0530

    updated

--- 01_Basic/01_Conventions.mjs
+++ 01_Basic/01_Conventions.mjs
@@ -3,3 +3,16 @@
     method, invoking decipher. Update with the ciphertext to decrypt and use decipher.final when we're done.
 */
 
+const crypto = require('crypto')
+const fs = require('fs')
+const util = require('util')
+const readFile = util.promisify(fs.readFile)
+
+const bobPublicKeyPem = bobKeyPair.publicKey.export(
+    {type: 'spki', format: 'pem'}
+)
+    
+const aliceSharedSecret = crypto.diffieHellman({
+    publicKey: crypto.createPublicKey(bobPublicKeyPem),
+    privateKey: aliceKeyPair.privateKey
+})
