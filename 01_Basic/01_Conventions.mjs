7d67ea698980f1a684d36abebdb27c4ec9c72481
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-01 19:01:42 +0530

    updated

---
+++ 01_Basic/01_Conventions.mjs
@@ -0,0 +1,5 @@
+/*
+    Here is an example: We use the decipher object, just like we did with the cipher object in the previous
+    method, invoking decipher. Update with the ciphertext to decrypt and use decipher.final when we're done.
+*/
+
